2896af7cc7b335f4509e3678346d5e6a64d672d6
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-21 14:58:19 +0700

    menambahkan destruktor untuk kelas mahasiswa

--- inisialitation.cpp
+++ inisialitation.cpp
@@ -11,6 +11,12 @@ class mahasiswa{
         mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai (pNilai){
             //member initialization definisi
         }
+    ~mahasiswa()
+    {
+        cout << "Id = "<< id << endl;
+        cout << "Nama = "<< nama << endl;
+        cout << "Nilai = "<< nilai << endl;
+    }
 
 
 };
