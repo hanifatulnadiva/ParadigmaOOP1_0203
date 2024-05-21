0a9d1becba5499642a75ff929422d3c0f0347a2a
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-21 14:55:24 +0700

    menambahkan member initialization

--- inisialitation.cpp
+++ inisialitation.cpp
@@ -8,6 +8,9 @@ class mahasiswa{
         string nama;
         float nilai;
     public:
+        mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai (pNilai){
+            //member initialization definisi
+        }
 
 
 };
